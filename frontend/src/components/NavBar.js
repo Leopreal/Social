9a785787e7ea66c7cc704ec1ca8f090f96ed07d5
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-10-07 14:47:15 -0300

    fazendo as alterações de autenticação na nav bar

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -6,10 +6,19 @@ import {
   BsSearch,
   BsHouseDoorFill,
   BsFillPersonFill,
-  BsFillCameraFill,
+  BsPostcard,
 } from "react-icons/bs";
 
+// hooks
+import { useState } from "react";
+import { useAuth } from "../hooks/useAuth";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const NavBar = () => {
+  const { auth } = useAuth();
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <nav id="nav">
       <Link to={"/"}>Social</Link>
@@ -18,17 +27,39 @@ const NavBar = () => {
         <input type="text" placeholder="pesquisar" />
       </form>
       <ul id="nav-links">
-        <li>
-          <NavLink to={"/"}>
-            <BsHouseDoorFill />
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to={"/login"}>Entrar</NavLink>
-        </li>
-        <li>
-          <NavLink to={"/register"}>Cadastrar</NavLink>
-        </li>
+        {auth ? (
+          <>
+            <li>
+              <NavLink to={"/"}>
+                <BsHouseDoorFill />
+              </NavLink>
+            </li>
+            {user && (
+              <li>
+                <NavLink to={`/users/${user._id}`}>
+                  <BsPostcard />
+                </NavLink>
+              </li>
+            )}
+            <li>
+              <NavLink to={"/profile"}>
+                <BsFillPersonFill />
+              </NavLink>
+            </li>
+            <li>
+              <span>Sair</span>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <NavLink to={"/login"}>Entrar</NavLink>
+            </li>
+            <li>
+              <NavLink to={"/register"}>Cadastrar</NavLink>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
